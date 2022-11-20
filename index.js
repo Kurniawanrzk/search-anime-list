c31a5ce8a19bac41396c540de6ceaedb8dccf796
Kurniawanrzk <kurniawanrz205@gmail.com>
2022-11-20 23:33:10 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const scrapeIt = require("scrape-it")
+
+const getListAnime = async(q) => {
+
+await scrapeIt(`https://myanimelist.net/search/all?q=${q}&cat=anime`, {
+	articles:{
+		listItem:"div.information.di-tc.va-t.pt4.pl8",
+		data:{
+			title:".hoverinfo_trigger.fw-b.fl-l",
+			content:".pt8.fs10.lh14.fn-grey4"
+		}
+	}
+}).then(({data, response}) => {
+	console.log(`Status Code : ${response.statusCode}`)
+	console.log(data)
+})
+
+}
+getListAnime('fairy tail')
+

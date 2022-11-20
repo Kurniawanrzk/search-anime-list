5fc6a440b858bd4a78bde46e238d7b01a6f4a5c2
KurniawanRizky <92316345+Kurniawanrzk@users.noreply.github.com>
2022-11-20 23:43:03 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,20 +4,18 @@ const scrapeIt = require("scrape-it")
 const getListAnime = async(q) => {
 
 await scrapeIt(`https://myanimelist.net/search/all?q=${q}&cat=anime`, {
-	articles:{
-		listItem:"div.information.di-tc.va-t.pt4.pl8",
-		data:{
-			title:".hoverinfo_trigger.fw-b.fl-l",
-			content:".pt8.fs10.lh14.fn-grey4"
-		}
-	}
+    articles:{
+	listItem:"div.information.di-tc.va-t.pt4.pl8",
+	  data:{
+	   title:".hoverinfo_trigger.fw-b.fl-l",
+	   content:".pt8.fs10.lh14.fn-grey4"
+	   }
+	 }
 }).then(({data, response}) => {
 	console.log(`Status Code : ${response.statusCode}`)
 	console.log(data)
 })
-
 }
-
 // Add The Anime Title As Parameter
 getListAnime('fairy tail')
 
